dbf9c3d7832b9a103b1e73100be3f131a8ad9bf0
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-05-25 14:23:31 +0600

    make admin

--- src/Pages/Header/Admin/AdminRow.js
+++ src/Pages/Header/Admin/AdminRow.js
@@ -3,10 +3,22 @@ import React from 'react';
 const adminRow = ({user,index}) => {
     console.log(user);
     const {email} = user;
+    const createAdmin=()=>{
+        // console.log('object');
+        fetch( `http://localhost:4000/user/admin/${email}`,{
+            method:'PUT',
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        }).then(res=>res.json()).then(data=> {
+            console.log(data);
+        })
+    }
     return (
         <tr>
             <th>{index +1}</th>
             <td>{email}</td>
+            <td><button className='btn' onClick={createAdmin}>Make Admin</button></td>
         </tr>
     );
 };
