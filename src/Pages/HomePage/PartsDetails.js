e185ef2f3839b7c89c59c4950bc5e19a7cfd579e
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-07-23 23:59:37 +0600

    h

--- src/Pages/HomePage/PartsDetails.js
+++ src/Pages/HomePage/PartsDetails.js
@@ -8,19 +8,20 @@ const PartsDetails = ({part}) => {
  
     return (
         <div>
-          
+              <div className='shadow-lg rounded-2xl  bg-gray-200 p-4 text-center'>
             <figure class="px-10  pt-10">
                 <img src={img} alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
                 <h2 class="card-title">{name}</h2>
                 <p>{description}</p>
-                <p>Minumum Buy: {minQuantity} </p>
+                <p>Minumum Buy:- {minQuantity} </p>
                 <p>Available Parts: {availableQuantity} </p>
                 <p>Price: ${price} </p>
                 <div class="card-actions">
                  <button onClick={()=> navigate(`/purchase/${part._id}`)} class="btn btn-primary"  > Buy Now </button>
                 </div>
+            </div>
             </div>
         </div>
     );
